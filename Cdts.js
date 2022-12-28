572b5c2f2de388cd2a7a73c20bbabd5649fc16d2
q6285917 <66327892+q6285917@users.noreply.github.com>
2022-12-29 05:15:20 +0800

    Update Cdts.js

--- Cdts.js
+++ Cdts.js
@@ -2,16 +2,6 @@
 ^http[s]?:\/\/xidi.sutanapp.com\/mobile\/user\/userIndex.+$ url script-response-body sutanapp.js
 [mitm] 
 hostname = *.sutanapp.*
-*******************************
-Surge
-
-[Script]
-^http[s]?:\/\/xidi.sutanapp.com\/mobile\/user\/userIndex.+$ requires-body=1,max-size=0,script-path=sutanapp.js
-
-[MITM]
-hostname = *.sutanapp.*
-
-*******************************/
 var obj = JSON.parse($response.body);
     obj.data.= 1,;
 obj.data.1= 1;
