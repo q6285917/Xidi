512b8940586fce9fea1ae6cb175e3bb7e4407138
q6285917 <66327892+q6285917@users.noreply.github.com>
2023-01-02 09:31:39 +0800

    Update Cdts.js

--- Cdts.js
+++ Cdts.js
@@ -1,14 +1,6 @@
-/*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-滴滴
-脚本名称:滴滴
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
-群1077223830
-*******************************
+
 [rewrite_local]
-^http[s]?:\/\/xidi.sutanapp.com\/\/mobile\/user\/userIndex.+$ url script-response-body sutanapp.js
+^http[s]?:\/\/xidi.sutanapp.com\/\/mobile\/user\/userIndex.+$ url script-response-body https://github.com/q6285917/Xidi/edit/main/Cdts.js
 [mitm] 
 hostname = *.sutanapp.*
 *******************************
