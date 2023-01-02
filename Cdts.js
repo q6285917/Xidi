e06764af0725df00dd0c1a2a877560fea258c5a1
q6285917 <66327892+q6285917@users.noreply.github.com>
2023-01-02 09:37:07 +0800

    Update Cdts.js

--- Cdts.js
+++ Cdts.js
@@ -3,18 +3,6 @@
 ^http[s]?:\/\/xidi.sutanapp.com\/\/mobile\/user\/userIndex.+$ url script-response-body https://github.com/q6285917/Xidi/edit/main/Cdts.js
 [mitm] 
 hostname = *.sutanapp.*
-*******************************
-Surge
-
-[Script]
-^http[s]?:\/\/xidi.sutanapp.com\/\/mobile\/user\/userIndex.+$ requires-body=1,max-size=0,script-path=sutanapp.js
-
-[MITM]
-hostname = *.sutanapp.*
-
-*******************************/
-var obj = JSON.parse($response.body);
-    obj.data.subVip= 1;
 obj.data.= 1,;
     $done({body: JSON.stringify(obj)});
 
